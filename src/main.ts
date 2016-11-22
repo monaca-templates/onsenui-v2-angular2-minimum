9e4dc138df57a6bdf89ff9fbe3321e6ea52f4cf8
Naoki Matagawa <naoki.m@asial.co.jp>
2016-11-22 18:49:25 +0900

    Fix `main.ts` and bump version to 2.2.2+20161122.3.

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ require('onsenui/css/onsenui.css');
 
 // Application code starts here
 import {enableProdMode, NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {HttpModule} from '@angular/http';
-import {RouterModule} from '@angular/router';
 import {OnsenModule} from 'angular2-onsenui';
 
 import {MyApp} from './app/app';
@@ -21,13 +19,9 @@ if (process.env.NODE_ENV === 'production') {
     imports: [
         OnsenModule,
         HttpModule,
-        RouterModule,
     ],
     declarations: [
         MyApp,
-    ],
-    providers: [
-        {provide: LocationStrategy, useClass: HashLocationStrategy},
     ],
     bootstrap: [
         MyApp,
